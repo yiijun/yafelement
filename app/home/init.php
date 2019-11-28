6f1f39c455788b766f4855362ef0a8ab47a236e4
tangyijun <tangyijun@021.com>
2019-11-28 17:34:09 +0800

    增加home

---
+++ app/home/init.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: tangyijun
+ * Date: 2019-11-26
+ * Time: 15:48
+ */
+define('APP_NAME','home');
+define('APP_PATH', dirname(__FILE__));
+define('SITE_NAME',$_SERVER['SERVER_NAME']);
+require __DIR__.'/../../vendor/autoload.php';
+$app = new \Yaf\Application("../../conf/".YAF\ENVIRON."/".APP_NAME.".ini");
---
+++ public/home/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * User: tangyijun
+ * Date: 2019-11-26
+ * Time: 15:46
+ */
+require __DIR__ . '/../../app/home/init.php';
+$app->bootstrap()->run();
